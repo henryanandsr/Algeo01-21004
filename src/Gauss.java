95c46e29b93c520ca02def3dac18e473c6beec7e
Fahrian <73996601+fchrgrib2310@users.noreply.github.com>
2022-09-23 21:38:34 +0700

    tools

--- src/Gauss.java
+++ src/Gauss.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class Gauss {
+    private static Operator tools = new Operator();
     public static double[][] matrixGauss(double[][] m){
         int baris = m.length;
         int kolom = m[0].length;
@@ -11,8 +12,8 @@ public class Gauss {
                     jumNol++;
                 }
             }
-            if((jumNol>i)&&(isNotAllNol(m,i,i))){
-                changeRow(m,i);
+            if((jumNol>i)&&(tools.isNotAllNol(m,i,i))){
+                tools.changeRow(m,i);
             }
         }
         for(int i=0;i<baris;i++){
@@ -25,54 +26,5 @@ public class Gauss {
             }
         }
         return m;
-    }
-    public static void changeRow(double[][] m,int idxRow1){
-        double[] copyRow1 = new double[m[0].length];
-        double[] copyRow2 = new double[m[0].length];
-        int idxR=0;
-        for(int x=0;x<2;x++){
-            if(x==0){
-                for(int i=idxRow1;i<m.length;i++){
-                    if(i==idxRow1){
-                        for(int k=0;k<m[0].length;k++){
-                            copyRow1[k] = m[i][k];
-                        }
-                    }
-                    if(m[i][idxRow1]!=0){
-                        for(int k=0;k<m[0].length;k++){
-                            copyRow2[k] = m[i][k];
-                            idxR=i;
-                        }
-                    }
-                }
-            }
-            if(x==1){
-                for(int i=idxRow1;i<m.length;i++){
-                    if(i==idxRow1){
-                        for(int k=0;k<m[0].length;k++){
-                            m[i][k]=copyRow2[k];
-                        }
-                    }
-                    if(i==idxR){
-                        for(int k=0;k<m[0].length;k++){
-                            m[i][k]=copyRow1[k];
-                        }
-                    }
-                }
-            }
-        }
-    }
-    public static boolean isNotAllNol(double[][] m,int idxRow,int idxCol){
-        boolean isNotNol = false;
-        int isNol=0;
-        for(int i=idxRow;i<m.length;i++){
-            if(m[i][idxCol]==0){
-                isNol++;
-            }
-        }
-        if(isNol!=0){
-            isNotNol = true;
-        }
-        return isNotNol;
     }
 }
--- src/Operator.java
+++ src/Operator.java
@@ -20,6 +20,64 @@ public class Operator {
             System.out.println("");
         }
     }
+    /*
+    fungsi changeRow adalah fungsi yang digunakan untuk menukar baris
+    yang dimana akan menukar jika kolom awal baris di bawah baris idxRow1 tidak sama dengan nol
+    */
+    public static void changeRow(double[][] m,int idxRow1){
+        double[] copyRow1 = new double[m[0].length];
+        double[] copyRow2 = new double[m[0].length];
+        int idxR=0;
+        for(int x=0;x<2;x++){
+            if(x==0){
+                for(int i=idxRow1;i<m.length;i++){
+                    if(i==idxRow1){
+                        for(int k=0;k<m[0].length;k++){
+                            copyRow1[k] = m[i][k];
+                        }
+                    }
+                    if(m[i][idxRow1]!=0){
+                        for(int k=0;k<m[0].length;k++){
+                            copyRow2[k] = m[i][k];
+                            idxR=i;
+                        }
+                    }
+                }
+            }
+            if(x==1){
+                for(int i=idxRow1;i<m.length;i++){
+                    if(i==idxRow1){
+                        for(int k=0;k<m[0].length;k++){
+                            m[i][k]=copyRow2[k];
+                        }
+                    }
+                    if(i==idxR){
+                        for(int k=0;k<m[0].length;k++){
+                            m[i][k]=copyRow1[k];
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /*
+    fungsi boolean isNotAllNol adalah fungsi yang digunakan untuk mencari
+    apakah dibaris ini bernilai nol setelah diagonalnya atau tidak
+    */
+    public static boolean isNotAllNol(double[][] m,int idxRow,int idxCol){
+        boolean isNotNol = false;
+        int isNol=0;
+        for(int i=idxRow;i<m.length;i++){
+            if(m[i][idxCol]==0){
+                isNol++;
+            }
+        }
+        if(isNol!=0){
+            isNotNol = true;
+        }
+        return isNotNol;
+    }
     public static double[][] bacaMatriks(int row, int col){
         double arr[][] = new double[row][col];
         Scanner s = new Scanner(System.in);
