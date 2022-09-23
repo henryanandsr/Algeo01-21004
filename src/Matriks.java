281c4878fa8fcfd95cfdadaa1de3b2821f3fc8b4
Fahrian <73996601+fchrgrib2310@users.noreply.github.com>
2022-09-23 21:21:39 +0700

    ini buat print matrix

--- src/Matriks.java
+++ src/Matriks.java
@@ -1,4 +1,4 @@
-import java.util.scanner;
+import java.util.Scanner;
 
 public class Matriks {
     public static double[][] bacaMatriks(int row, int col){
--- src/Operator.java
+++ src/Operator.java
@@ -1,3 +1,15 @@
 public class Operator {
-    //masukin fungsi tambahan di sini
+    public static void printMatrix(double[][] m){
+        for(int i=0;i<m.length;i++){
+            for(int j=0;j<m[0].length;j++){
+                if(j==0){
+                    System.out.print(m[i][j]);
+                }
+                if(j>0){
+                    System.out.print(m[i][j]);
+                }
+            }
+            System.out.println("");
+        }
+    }
 }
