a31232b67c35d78cbda37e28b3dd2f986aa2cc9c
christodharma <13521009@std.stei.itb.ac.id>
2022-09-23 17:10:34 +0700

    file untuk fungsi input matriks

--- src/Matriks.java
+++ src/Matriks.java
@@ -1,11 +1,11 @@
 import java.util.scanner;
 
 public class Matriks {
-    public static double[][] bacaMatriks(int brs, int kol){
-        double arr[][] = new double[brs][kol];
+    public static double[][] bacaMatriks(int row, int col){
+        double arr[][] = new double[row][col];
         Scanner s = new Scanner(System.in);
-        for (int i = 0; i < brs; ++i){
-            for (int j = 0; j < kol; ++j){
+        for (int i = 0; i < row; ++i){
+            for (int j = 0; j < col; ++j){
                 arr[i][j] = s.nextDouble();
             }
         }
