e3e086c50849a4d968f3aea0cb29e00935fe4883
henryanandsr <henryanandseptianradityo@gmail.com>
2022-09-24 23:46:56 +0700

    add inverse function

--- src/Operator.java
+++ src/Operator.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 import static java.lang.Math.abs;
 
+
 public class Operator {
     /*
     prosedur ini berfungsi untuk print matrix
@@ -9,6 +10,14 @@ public class Operator {
 
     print ini akan menampilakan matrix sesuai dengan baris dan kolomnya
     */
+    public static void main(String[] args) {
+        double[][] m = {{1,2,3},{4,5,6},{7,8,9}};
+        Operator.printMatrix(sub_matriks(m,0,0));
+        // System.out.println(m.length);
+        // System.out.print(m[0].length + "\n");
+        // System.out.println(determinan(m) + "\n");
+        Operator.printMatrix(adjoin(m));
+    }
     public static void printMatrix(double[][] m){
         for(int i=0;i<m.length;i++){
             for(int j=0;j<m[0].length;j++){
@@ -97,5 +106,70 @@ public class Operator {
             }
         }
         return arr;
+    }
+    public static double[][] sub_matriks(double[][] arr, int brs, int kol){
+        double sub[][] = new double[(arr.length)-1][(arr[0].length)-1];
+        int i,j;
+        int idbrs = 0;
+        int idkol = 0;
+        for(i=0;i<arr.length;i++){
+            for(j=0;j<arr[0].length;j++){
+                if (i!=brs && j!=kol){
+                    sub[idbrs][idkol] = arr[i][j];
+                    if (idkol==(sub[0].length-1)){
+                        idbrs+=1;
+                        idkol=0;
+                    } else{
+                        idkol +=1;
+                    }
+                } 
+            }
+        }
+        return sub;
+    }
+    public static double determinan(double[][] arr){
+        int kali = 1;
+        double det = 0;
+        int i;
+        if (arr.length==1){
+            return arr[0][0];
+        }
+        for (i=0;i<arr[0].length;i++){
+            det += (arr[0][i] * kali * determinan(sub_matriks(arr,0,i)));
+            kali *=-1;
+        }
+        return det;
+    }
+    public static double[][] copyMatriks(double[][] arr, double[][] copy){
+        int i,j;
+        for (i=0;i<arr.length;i++){
+            for(j=0;j<arr[0].length;j++){
+                copy[i][j] = arr[i][j];
+            }
+        }
+        return copy;
+    }
+    public static void transpose(double[][] arr){
+        int i,j;
+        double copy[][] = new double[arr.length][arr[0].length];
+        copyMatriks(arr, copy);
+        for(i=0;i<arr.length;i++){
+            for(j=0;j<arr[0].length;j++){
+                arr[i][j] = copy[j][i];
+            }
+        }
+    }
+    public static double[][] adjoin(double[][] arr){
+        int i,j;
+        int kali = 1;
+        double copy[][] = new double[arr.length][arr[0].length];
+        for (i=0;i<arr.length;i++){
+            for(j=0;j<arr[0].length;j++){
+                copy[i][j] = kali*determinan(sub_matriks(arr, i, j));
+                kali *= -1;
+            }
+        }
+        transpose(copy);
+        return copy;
     }
 }
