64f294df5d3ed3a3f205f58455f70b8fd1ee669a
christodharma <13521009@std.stei.itb.ac.id>
2022-09-28 22:59:25 +0700

    Cramer selesai

--- src/Cramer.java
+++ src/Cramer.java
@@ -1,26 +1,20 @@
  public class Cramer{
-    public static void main(String[] args){
-        double[][] m = {{2,1,5},{4,7,5}};
-        double[] result = cramer(m);
-        for (int i=0;i<result.length;i++) {
-            System.out.println(result[i]);
-        }
-    }
     public static double[] cramer(double[][] A) {
-        double[] result = new double[A[0].length-1];
-        double copyA[][] = Operator.getMatrix(A,0,0,A.length-1,A[0].length-2);
-        double detA = DeterminanKofaktor.determinanKofaktor(copyA);
-        System.out.println(copyA);
-        Operator.printMatrix((copyA));
-        for (int j=0;j<copyA[0].length;j++){
-            result[j] = DeterminanKofaktor.determinanKofaktor(cramerInsert(A,j))/detA;
+        double mainA[][] = Operator.getMatrix(A,0,0,A.length-1,A[0].length-2);
+        double[] result = new double[mainA[0].length];
+        double detA = DeterminanKofaktor.determinanKofaktor(mainA);
+        double[][] b = Operator.getMatrix(A, 0, A[0].length-1, A.length-1, A[0].length-1);
+        for (int j=0;j<mainA[0].length;j++){
+            result[j] = DeterminanKofaktor.determinanKofaktor(cramerInsert(mainA,b,j))/detA;
         }
         return result;
     }
-    private static double[][] cramerInsert(double[][] arr, int col) {
+    private static double[][] cramerInsert(double[][] arr, double[][] b,int col) {
+        double[][] copy = new double[arr.length][arr[0].length];
+        Operator.copyMatriks(arr, copy);
         for (int i=0;i<arr.length;i++) {
-            arr[i][col] = arr[i][arr[0].length-1];
+            copy[i][col] = b[i][0];
         }
-        return arr;
+        return copy;
     }
 }
