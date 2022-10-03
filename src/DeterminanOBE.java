be4834a5b71084ad143fffb58eb6569f514e3ccc
christodharma <13521009@std.stei.itb.ac.id>
2022-10-03 17:14:33 +0700

    determinanOBE

--- src/DeterminanOBE.java
+++ src/DeterminanOBE.java
@@ -1,20 +1,6 @@
 import static java.lang.Math.abs;
 
 public class DeterminanOBE {
-    public static void main(String[] args){
-        double [][] m ={
-            {1,2,3},
-            {4,5,6},
-            {7,8,9}
-        };
-        // double[][] m={
-        //     {0,0,2,-1},
-        //     {2,0,-2,-2},
-        //     {-1,2,-4,1},
-        //     {3,0,0,-3}
-        // };
-        System.out.println(determinan(m));
-    }
     private static int sign=1;
     private static double constant=1;
     private static double det;
