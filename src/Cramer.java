9ab37f2a7a930bda557d7b84c8bfed8cc5b52d32
christodharma <13521009@std.stei.itb.ac.id>
2022-10-03 09:17:47 +0700

    Cramer selesai

--- src/Cramer.java
+++ src/Cramer.java
@@ -1,22 +1,4 @@
  public class Cramer{
-    public static void main(String[] args){
-    double[][] m = {
-        {0,0,2,-1},
-        {2,0,-2,-2},
-        {-1,2,-4,1},
-    };
-    // double[][] m = {
-    //     {0,0,2,-1,4},
-    //     {2,0,-2,-2,3},
-    //     {-1,2,-4,1,2},
-    //     {3,0,0,-3,1}
-    // };
-    double[] x= new double[m.length];
-    for (int i=0;i<m.length;i++){
-        x[i] = cramer(m)[i];
-        System.out.print(x[i]+" ");
-    }
-    }
     public static double[] cramer(double[][] m) {
         double detA;
         double[][] A = Operator.getMatrix(m,0,0,m.length-1,m[0].length-2);
@@ -24,7 +6,7 @@
         double[] result = new double[A[0].length];
         if (A.length>=1 && A.length<4){
             double detInsert;
-            double[][] inserted = new double[A.length][A.length];
+            double[][] inserted = new double[A.length][A[0].length];
             detA = DeterminanKofaktor.determinan(A);
             for (int j=0;j<A[0].length;j++){
                 inserted = insertBtoA(A, b, j);
@@ -34,13 +16,11 @@
         } else if (m.length>=4){
             detA = DeterminanOBE.determinan(A);
             double detInsert;
-            double[][] inserted;
-            int j = 0;
-            while (j<A[0].length){
+            double[][] inserted = new double[A.length][A[0].length];
+            for (int j=0;j<A[0].length;j++){
                 inserted = insertBtoA(A, b, j);
-                detInsert = DeterminanOBE.determinan(inserted);
+                detInsert = DeterminanKofaktor.determinan(inserted);
                 result[j] = detInsert/detA;
-                j++;
             }
         }
         return result;
--- src/DeterminanOBE.java
+++ src/DeterminanOBE.java
@@ -1,20 +1,11 @@
 import static java.lang.Math.abs;
 
 public class DeterminanOBE {
-    public static void main(String[] args) {  
-        double[][] m={
-            {0,0,2,-1},
-            {2,0,-2,-2},
-            {-1,2,-4,1},
-            {3,0,0,-3}
-        };
-        System.out.println(determinan(m));
-    }
-    public static int sign=1;
-    public static double constant=1;
-    public static double det;
+    private static int sign=1;
+    private static double constant=1;
+    private static double det;
     //fungsi yang memeriksa jika ada baris/kolom berisi 0 semua (determinan=0)
-    public static boolean HasAll0RowCol(double[][] m){
+    private static boolean HasAll0RowCol(double[][] m){
         for (int i=0;i<m.length;i++){
             if (All0rowcol(m,true,i)){
                 return true;
@@ -26,7 +17,7 @@ public class DeterminanOBE {
         return false;
     }
     //Fungsi mengecek jika baris/kolom bernilai 0 semua
-    public static boolean All0rowcol(double[][] m, boolean isRow, int idx){
+    private static boolean All0rowcol(double[][] m, boolean isRow, int idx){
         double[] temp = new double[m.length];
         if (isRow){
             for (int i=0;i<temp.length;i++){
@@ -45,20 +36,20 @@ public class DeterminanOBE {
         return true;
     }
     //Fungsi mengurangi baris ke brs1 dengan brs2
-    public static void kurangBasis(double[][] m, int row, double[] basis){
+    private static void kurangBasis(double[][] m, int row, double[] basis){
         int i;
         for(i=0;i<m[0].length;i++){
             m[row][i] = m[row][i] - basis[i];
         }
     }
-    public static void tambahBasis(double[][] m, int row, double[] basis){
+    private static void tambahBasis(double[][] m, int row, double[] basis){
         int i;
         for(i=0;i<m[0].length;i++){
             m[row][i] = m[row][i] + basis[i];
         }
     }
     // fungsi mencetak baris yang sekarang menjadi basis
-    public static void printBasis(double[] basis){
+    private static void printBasis(double[] basis){
         for(int j=0;j<basis.length;j++){
             if(j==0){
                 if(basis[j]==0){
@@ -78,14 +69,14 @@ public class DeterminanOBE {
         System.out.println();
     }
     //Fungsi mengalikan suatu baris dengan n
-    public static void rowKaliConst(double[][] arr, int brs, double n){
+    private static void rowKaliConst(double[][] arr, int brs, double n){
         int i;
         for(i=0;i<arr[0].length;i++){
             arr[brs][i] = arr[brs][i] * n;
         }
     }
     //Fungsi menukar brs1 dan brs2 pada matrix
-    public static void swapRow(double[][] arr, int brs1, int brs2){
+    private static void swapRow(double[][] arr, int brs1, int brs2){
         double copy[][] = new double[arr.length][arr[0].length];
         copy = Operator.copyMatriks(arr, copy);
         int i;
@@ -95,7 +86,7 @@ public class DeterminanOBE {
         }
     }
 
-    public static void obe(double[][] m){
+    private static void obe(double[][] m){
     double basis[] = new double[m[0].length];
     for (int idxBasis=0;idxBasis<m.length;idxBasis++){   //idxBasis = row basis sekarang
         //swap basis ke baris non 0 kalau 0 (kalau ngga, lanjut)
@@ -106,34 +97,21 @@ public class DeterminanOBE {
             }
             swapRow(m,idxBasis,scanNot0);
             sign*=-1;
-// System.out.println("terjadi swap baris " + (idxBasis+1) + " dan " + (scanNot0+1));
         }
-// Operator.printMatrix(m);
-// System.out.println();
-        //mengisi list basis dengan elemen baris dan disederhanakan
-        //penyederhanaan digunakan untuk eliminasi elmt di bawah basis
         for (int i=0;i<m[0].length;i++){
             basis[i]=m[idxBasis][i]/m[idxBasis][idxBasis];
         }
-// System.out.println("Basis:");
-// printBasis(basis);
-        //eliminasi 1 column di bawah elemen basis
-        //dengan pengurangan baris tsb dengan basis
         int elimRow = idxBasis+1;
         while (elimRow<m.length) {
-// System.out.println("\niterasi eliminasi "+elimRow);
             while(m[elimRow][idxBasis]!=0){
                 if (m[elimRow][idxBasis]>0 && m[elimRow][idxBasis]<1) {
                     constant *= 1/m[elimRow][idxBasis];
                     rowKaliConst(m, elimRow, 1/m[elimRow][idxBasis]);
-// System.out.println(constant);
                 } else if (m[elimRow][idxBasis]>0) {
                     kurangBasis(m, elimRow, basis);
                 } else { //m[elimRow][idxBasis] < 0
                     tambahBasis(m, elimRow, basis);
                 }
-// Operator.printMatrix(m);
-// System.out.println("'terkurangi' 1");
             }
             elimRow++;
         }
@@ -150,9 +128,8 @@ public class DeterminanOBE {
             for (int i=0;i<m.length;i++){
                 det = det * mCopy[i][i];
             }   
+            det = det*sign/constant;
         }
-        det = det*sign/constant;
-// System.out.println(det + "*" + sign + "/" + constant); 
         return det;
     }
 }
